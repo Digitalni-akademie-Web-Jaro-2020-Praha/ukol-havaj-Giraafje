997b24711120e847d60f8ce15af167f43e1e8d76
Lenka Erbenova <lenka.erbenova79@gmail.com>
2020-06-05 22:45:01 +0200

    Podmínky vloženy do funkce a funkce otestována

--- index.js
+++ index.js
@@ -7,28 +7,43 @@ let osoba2 = {
   uspory: 68000
 };
 
-if (osoba1.uspory + osoba2.uspory >= 100000) {
-  console.log("Hurá na dovolenou!")
-  if (osoba1.uspory >= 50000 && osoba2.uspory >= 50000) {
-    console.log("Obě máte dostatek.")
+let osoba3 = {
+  jmeno: 'Lenka',
+  uspory: 20000
+};
+let osoba4 = {
+  jmeno: 'Petra',
+  uspory: 40000
+};
+
+const canWeGo = (prvniOsoba, druhaOsoba) => {
+  if (prvniOsoba.uspory + druhaOsoba.uspory >= 100000) {
+    console.log("Hurá na dovolenou!")
+    if (prvniOsoba.uspory >= 50000 && druhaOsoba.uspory >= 50000) {
+      console.log("Obě máte dostatek.")
+    }
+    else if (prvniOsoba.uspory < 50000) {
+      console.log(prvniOsoba.jmeno, "ještě nemá dostatek, chybí", (50000 - prvniOsoba.uspory), ".")
+    }
+    else if (druhaOsoba.uspory < 50000) {
+      console.log(druhaOsoba.jmeno, "ještě nemá dostatek, chybí", (50000 - druhaOsoba.uspory), ".")
+    }
   }
-  else if (osoba1.uspory < 50000) {
-    console.log(osoba1.jmeno, "ještě nemá dostatek, chybí", (50000 - osoba1.uspory), ".")
-  }
-  else if (osoba2.uspory < 50000) {
-    console.log(osoba2.jmeno, "ještě nemá dostatek, chybí", (50000 - osoba2.uspory), ".")
+  else {
+    console.log("Celkem chybí", (100000 - (prvniOsoba.uspory + druhaOsoba.uspory)), ".")
+    if (prvniOsoba.uspory < 50000 && druhaOsoba.uspory < 50000) {
+      console.log(prvniOsoba.jmeno, "musí ještě naspořit", (50000 - prvniOsoba.uspory), ".")
+      console.log(druhaOsoba.jmeno, "musí ještě naspořit", (50000 - druhaOsoba.uspory), ".")
+    }
+    else if (prvniOsoba.uspory < 50000) {
+      console.log(prvniOsoba.jmeno, "musí ještě naspořit", (50000 - prvniOsoba.uspory), ".")
+    }
+    else if (druhaOsoba.uspory < 50000) {
+      console.log(druhaOsoba.jmeno, "musí ještě naspořit", (50000 - druhaOsoba.uspory), ".")
+    }
   }
 }
-else {
-  console.log("Celkem chybí", (100000 - (osoba1.uspory + osoba2.uspory)), ".")
-  if (osoba1.uspory < 50000 && osoba2.uspory < 50000) {
-    console.log(osoba1.jmeno, "musí ještě naspořit", (50000 - osoba1.uspory), ".")
-    console.log(osoba2.jmeno, "musí ještě naspořit", (50000 - osoba2.uspory), ".")
-  }
-  else if (osoba1.uspory < 50000) {
-    console.log(osoba1.jmeno, "musí ještě naspořit", (50000 - osoba1.uspory), ".")
-  }
-  else if (osoba2.uspory < 50000) {
-    console.log(osoba2.jmeno, "musí ještě naspořit", (50000 - osoba2.uspory), ".")
-  }
-}
+
+canWeGo(osoba1, osoba2)
+canWeGo(osoba1, osoba3)
+canWeGo(osoba2, osoba4)
